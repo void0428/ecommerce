d5e15f17963078afcb38e6fec6e21952e7bf2f66
Balaji Bandgar <balajibandgar26@gmail.com>
2025-11-21 03:02:46 +0530

    setting up frontend

--- backend/ecommerce_backend/settings.py
+++ backend/ecommerce_backend/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://fitzone-ecommerce.vercel.app',
+    'https://fitzone-woad.vercel.app/',
 ]
 
 ROOT_URLCONF = 'ecommerce_backend.urls'
@@ -184,7 +184,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://fitzone-ecommerce.vercel.app",
+    "https://fitzone-woad.vercel.app/",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
